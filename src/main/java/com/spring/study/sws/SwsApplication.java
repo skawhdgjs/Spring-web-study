ba7f77d2e3764c132ae0235bdacf5cd0f10cdd63
jongheon.nam <jongheon.nam@linecorp.com>
2019-07-17 15:12:21 +0900

    First commit

---
+++ src/main/java/com/spring/study/sws/SwsApplication.java
@@ -0,0 +1,13 @@
+package com.spring.study.sws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SwsApplication.class, args);
+    }
+
+}
